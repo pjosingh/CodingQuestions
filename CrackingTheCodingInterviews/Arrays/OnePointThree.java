4456592790bac9d426511fad0307cebadfdc23f0
pjosingh <singh.prabhjot@outlook.com>
2022-01-11 12:01:42 -0800

    Create OnePointThree.java

---
+++ CrackingTheCodingInterviews/Arrays/OnePointThree.java
@@ -0,0 +1,20 @@
+package CrackingTheCodingInterviews.Arrays;
+
+public class OnePointThree {
+
+    // remove duplicate characters from char arr
+
+    private static void removeDuplicates(char [] target) {
+        
+    }
+    public static void main(String ar[]) {
+        runTestCase("abcda");
+    }
+
+    private static void runTestCase(String target) {
+        char [] sample1 = target.toCharArray();
+        removeDuplicates(sample1);
+        System.out.println(sample1);
+    }
+    
+}
