44243de8a41318e16d9faa2e019e41696a101154
pjosingh <singh.prabhjot@outlook.com>
2022-01-17 08:14:36 -0800

    Update FilterExperiment.java

--- explore/FilterExperiment.java
+++ explore/FilterExperiment.java
@@ -2,9 +2,16 @@ package explore;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
 
 class FilterExperiment {
 
@@ -20,13 +27,25 @@ class FilterExperiment {
         .collect(Collectors.toList());
 
         System.out.println("Size: "+temp.size());
-        
-        return latestSignals
+        // Collection<User> latestVersions = users.stream()
+        //     .collect(Collectors.groupingBy(User::getUserId,
+        //             Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparing(User::getVersionNumber)), Optional::get)))
+        //             .values();
+
+
+        latestSignals = new ArrayList<> (latestSignals
                 .stream()
                 .filter(signal -> isTerminalSignalStatus(signal.getStatus()))
                 .filter(signal -> signal.getLastModifiedDate().isAfter(Instant.now().minusSeconds(ACTIONABLE_SIGNALS_TIME_THRESHOLD)))
-                .distinct()
-                .collect(Collectors.toList());
+                .collect(Collectors.toMap(Signal::getSignalId, Function.identity(), BinaryOperator.maxBy(Comparator.comparing(Signal::getSignalVersion)))).values());
+        Collection<Signal> latestVersion = latestSignals
+        .stream()
+        .collect(Collectors.groupingBy(Signal::getSignalId, Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparing(Signal::getSignalVersion)), Optional::get))).values();
+        
+        print(new LinkedList<>(latestVersion));
+        System.out.println("====");
+        return latestSignals;
+
     }
 
     private static boolean isTerminalSignalStatus(String target) {
@@ -70,12 +89,20 @@ class FilterExperiment {
     }
 }
 
-class Signal implements Comparable {
+class Signal {
     String signalId;
     int version;
     String signalStatus;
     String type;
     Instant lastModifiedDate;
+
+    public String getSignalId() {
+        return signalId;
+    }
+
+    public int getSignalVersion() {
+        return version;
+    }
 
     public String getStatus() {
         return signalStatus;
@@ -94,12 +121,6 @@ class Signal implements Comparable {
         response.type = signal.type;
         response.version = signal.version;
         return response;
-    }
-
-    @Override
-    public int compareTo(Object o) {
-        // TODO Auto-generated method stub
-        return 0;
     }
 
 }
